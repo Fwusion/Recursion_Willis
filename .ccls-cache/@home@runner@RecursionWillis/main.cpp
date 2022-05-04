ef7c2a5013c37bf62919457c36591aec79627886
Isaiah Willis <102613759+Fwusion@users.noreply.github.com>
2022-05-04 18:04:56 +0000

    Created and filled main, added input validation to make sure the user in only inputting an integer, and added "clear" to clean the console to only show the program's output.

--- .ccls-cache/@home@runner@RecursionWillis/main.cpp
+++ .ccls-cache/@home@runner@RecursionWillis/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include "Input_Validation_Extended.h"
 using namespace std;
 
 int tower(int num)
@@ -14,7 +15,13 @@ int tower(int num)
 
 int main()
 {
+  int num, sum;
+  system("clear"); //added because the stuff at the top of the console bothers me.
+  cout << "Please enter a number: ";
+  num = validateInt(num);
 
-
+  sum = tower(num);
+  cout << "The Factoral of " << num << " is: " << sum << endl; 
+  
   return 0;
 }
